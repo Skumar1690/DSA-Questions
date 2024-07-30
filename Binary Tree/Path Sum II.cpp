e9c6d27bf36526b72c1356d0de26b55f94f97d39
Saurabh Kumar <111876148+Skumar1690@users.noreply.github.com>
2024-07-31 03:12:51 +0530

    Create Path Sum II.cpp

---
+++ Binary Tree/Path Sum II.cpp
@@ -0,0 +1,33 @@
+// here intuition of becktracking 1) kya mei leaf pr hu  (left == null & right == null) sum ==  target , it mean good store this ,
+// if i not at leaf then call recursion on left and right and then backtack of initial valve tha store ;
+// lets go by code
+
+class Solution {
+public:
+vector<vector<int>>res;
+
+void solve(TreeNode* root , int sum , vector<int>& temp , int target){
+    if(root == NULL) return ;
+    sum += root->val;
+    temp.push_back(root->val);
+    if(root->left == NULL && root->right == NULL){
+        if(sum == target) {
+            res.push_back(temp);
+        }
+      
+    }
+    else {
+        solve(root->left , sum , temp , target);
+    solve(root->right ,sum , temp , target);
+    }
+    temp.pop_back();
+}
+
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        int sum =0;
+        vector<int>temp;
+        solve(root , sum , temp , targetSum);
+
+        return res;
+    }
+};
